064649a54e47c63d1e0e503ff9b8fc0f2f60d053
AgniNET <sushmakumariary9534@gmail.com>
2025-06-16 06:57:38 +0530

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,13 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_to_telegram(message: str):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": message}
+    requests.post(url, data=data)
